8d54e0f03d7c297105857dba2f3d815886b3a5f3
Kelsey Roy <kroy@8thlight.com>
2023-04-26 11:41:08 -0700

    Passes first degenerate test for EvaluateGuess

--- Wordle.Domain/Wordle.cs
+++ Wordle.Domain/Wordle.cs
@@ -3,5 +3,9 @@ public class Game
 {
     public string GetGreeting() => "Hello!";
 
+    public Score[] EvaluateGuess(string answer, string guess){
+        Score[] result = {Score.NotInWord, Score.NotInWord, Score.NotInWord, Score.NotInWord, Score.NotInWord};
+        return result;
+    }
 
 }
--- Wordle.Tests/WordleUnitTest.cs
+++ Wordle.Tests/WordleUnitTest.cs
@@ -1,9 +1,32 @@
 using Xunit;
+using Wordle.Domain;
+using static Wordle.Domain.Game;
+
 
 namespace Wordle.Tests;
 
 public class WordleUnitTests
 {
+    public static string answer = "APPLY";
+
+
     [Fact]
     public void PassingTest() => Assert.True(true);
+
+    [Fact]
+    public void EvaluateGuess_ShouldReturnAllNotInWordNoLetterIsInWord(){
+        var guess = "GROUT";
+        var game = new Wordle.Domain.Game();
+        Score[] expected_result = {
+            Score.NotInWord,
+            Score.NotInWord,
+            Score.NotInWord,
+            Score.NotInWord,
+            Score.NotInWord
+        };
+
+        var actual_result = game.EvaluateGuess(answer, guess);
+
+        Assert.Equal(expected_result, actual_result);
+    }
 }
