ec413de9ca7d526c1b07a878d9feaed3cbe71b07
Kelsey Roy <kroy@8thlight.com>
2023-04-27 11:08:52 -0700

    Updates .gitignore to ignore binary/bin files

--- Wordle.UI/Program.cs
+++ Wordle.UI/Program.cs
@@ -1,5 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Wordle.Domain;
 
-var wordle = new Wordle.Domain.Game();
-Console.WriteLine(wordle.GetGreeting());
+try 
+{
+    var wordle = new Wordle.Domain.Game();
+    Console.WriteLine(wordle.GetGreeting());
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
