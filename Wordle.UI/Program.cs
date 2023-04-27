9abd526b75eb077425bec4ad04438833e7ed5ac4
Kelsey Roy <kroy@8thlight.com>
2023-04-27 13:05:10 -0700

    Adds Main method to ensure dotnet build can run

--- Wordle.UI/Program.cs
+++ Wordle.UI/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
 using Wordle.Domain;
+
+namespace Wordle.UI;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        try {
+            Console.WriteLine("Hello");
+        }
+        catch (Exception error)
+        {
+            Console.WriteLine(error.Message);
+        }
+        
+    }
+}
