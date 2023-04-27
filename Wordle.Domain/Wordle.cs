400c2a3f1c4fb7ce0e6cbbec639128dfb8be9867
Kelsey Roy <kroy@8thlight.com>
2023-04-27 10:38:45 -0700

    Adds
    - conversion for lowercase letters to all uppercase plus tests
    - functionality and tests for instances of all InWord and all Correct
    - two if statements, replaces else if statement

--- Wordle.Domain/Wordle.cs
+++ Wordle.Domain/Wordle.cs
@@ -1,10 +1,33 @@
-﻿namespace Wordle.Domain;
+﻿using System;
+
+namespace Wordle.Domain;
 public class Game
 {
     public string GetGreeting() => "Hello!";
 
-    public Score[] EvaluateGuess(string answer, string guess){
-        Score[] result = {Score.NotInWord, Score.NotInWord, Score.NotInWord, Score.NotInWord, Score.NotInWord};
+    public Score[] EvaluateGuess(string answer, string guess)
+    {
+        Score[] result = {
+            Score.NotInWord,
+            Score.NotInWord,
+            Score.NotInWord,
+            Score.NotInWord,
+            Score.NotInWord
+        };
+        int i = 0;
+        foreach (char guessLetter in guess.ToUpper())
+        {
+            if (answer.Contains(guessLetter))
+            {
+                result[i] = Score.InWord;
+                if (answer[i] == guessLetter)
+                {
+                    result[i] = Score.Correct;
+                }
+            }
+            i++;
+        }
+
         return result;
     }
 
--- Wordle.Tests/WordleUnitTest.cs
+++ Wordle.Tests/WordleUnitTest.cs
@@ -2,20 +2,19 @@ using Xunit;
 using Wordle.Domain;
 using static Wordle.Domain.Game;
 
-
 namespace Wordle.Tests;
 
 public class WordleUnitTests
 {
-    public static string answer = "APPLY";
-
+    public static string answer = "ADEPT";
 
     [Fact]
     public void PassingTest() => Assert.True(true);
 
     [Fact]
-    public void EvaluateGuess_ShouldReturnAllNotInWordNoLetterIsInWord(){
-        var guess = "GROUT";
+    public void EvaluateGuess_WhenNoLetterIsInWord_ShouldReturnAllNotInWord()
+    {
+        var guess = "GROWN";
         var game = new Wordle.Domain.Game();
         Score[] expected_result = {
             Score.NotInWord,
@@ -23,6 +22,113 @@ public class WordleUnitTests
             Score.NotInWord,
             Score.NotInWord,
             Score.NotInWord
+        };
+
+        var actual_result = game.EvaluateGuess(answer, guess);
+
+        Assert.Equal(expected_result, actual_result);
+    }
+
+    [Fact]
+    public void EvaluateGuess_WhenFirstLetterIsCorrect_ShouldReturnOneCorrect()
+    {
+        var guess = "ARBOR";
+        var game = new Wordle.Domain.Game();
+        Score[] expected_result = {
+            Score.Correct,
+            Score.NotInWord,
+            Score.NotInWord,
+            Score.NotInWord,
+            Score.NotInWord
+        };
+
+        var actual_result = game.EvaluateGuess(answer, guess);
+
+        Assert.Equal(expected_result, actual_result);
+    }
+
+    [Fact]
+    public void EvaluateGuess_WhenFirstLetterIsInWord_ShouldReturnOneInWord()
+    {
+        var guess = "TOURS";
+        var game = new Wordle.Domain.Game();
+        Score[] expected_result = {
+            Score.InWord,
+            Score.NotInWord,
+            Score.NotInWord,
+            Score.NotInWord,
+            Score.NotInWord
+        };
+
+        var actual_result = game.EvaluateGuess(answer, guess);
+
+        Assert.Equal(expected_result, actual_result);
+    }
+
+    [Fact]
+    public void EvaluateGuess_WhenOneLetterIsCorrectAndOneLetterIsInWord_ShouldReturnOneCorrectOneInWord()
+    {
+        var guess = "AUDIO";
+        var game = new Wordle.Domain.Game();
+        Score[] expected_result = {
+            Score.Correct,
+            Score.NotInWord,
+            Score.InWord,
+            Score.NotInWord,
+            Score.NotInWord
+        };
+
+        var actual_result = game.EvaluateGuess(answer, guess);
+
+        Assert.Equal(expected_result, actual_result);
+    }
+
+    [Fact]
+    public void EvaluateGuess_WhenPlayersEnterAnyCase_ShouldStillEvaluateCorrectly()
+    {
+        var guess = "audio";
+        var game = new Wordle.Domain.Game();
+        Score[] expected_result = {
+            Score.Correct,
+            Score.NotInWord,
+            Score.InWord,
+            Score.NotInWord,
+            Score.NotInWord
+        };
+
+        var actual_result = game.EvaluateGuess(answer, guess);
+
+        Assert.Equal(expected_result, actual_result);
+    }
+
+    [Fact]
+    public void EvaluateGuess_WhenAllLettersAreInWord_ShouldReturnAllInWord()
+    {
+        var guess = "TAPED";
+        var game = new Wordle.Domain.Game();
+        Score[] expected_result = {
+            Score.InWord,
+            Score.InWord,
+            Score.InWord,
+            Score.InWord,
+            Score.InWord
+        };
+
+        var actual_result = game.EvaluateGuess(answer, guess);
+
+        Assert.Equal(expected_result, actual_result);
+    }
+    [Fact]
+    public void EvaluateGuess_WhenAllLettersAreCorrect_ShouldReturnAllCorrect()
+    {
+        var guess = "ADEPT";
+        var game = new Wordle.Domain.Game();
+        Score[] expected_result = {
+            Score.Correct,
+            Score.Correct,
+            Score.Correct,
+            Score.Correct,
+            Score.Correct
         };
 
         var actual_result = game.EvaluateGuess(answer, guess);
