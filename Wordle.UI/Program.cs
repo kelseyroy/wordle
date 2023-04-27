501688f02639095b5b00b00d6d1befd7ba47eaac
Kelsey Roy <kroy@8thlight.com>
2023-04-27 12:36:57 -0700

    Updated gitignore file

--- Wordle.UI/Program.cs
+++ Wordle.UI/Program.cs
@@ -1,12 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Wordle.Domain;
-
-try 
-{
-    var wordle = new Wordle.Domain.Game();
-    Console.WriteLine(wordle.GetGreeting());
-}
-catch(Exception e)
-{
-    Console.WriteLine(e.Message);
-}
