151a524e19694e154f886f97991494d8c8ee0f1b
Kelsey Roy <kroy@8thlight.com>
2023-05-10 09:35:18 -0600

    filepath should be working

--- Wordle.Tests/AnswerUnitTests.cs
+++ Wordle.Tests/AnswerUnitTests.cs
@@ -15,17 +15,10 @@ namespace Wordle.Tests;
 public class AnswerUnitTests
 {
     Answer answer = new Answer();
-    // public static string path = Path.Combine(Environment.CurrentDirectory, "/Wordle.Tests/Data/words_test.txt");
-    // public static String exePath = new Uri(Assembly.GetExecutingAssembly().Location).LocalPath;
-    public static string filePath = "/Users/kroy/git/wordle/Wordle.Tests/Data/words_test.txt";
-            //  Environment.CurrentDirectory;
-    public static string currentDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-    public static string path = Path.Combine(currentDirectory, filePath);
-    // public static string path = Path.Combine(Environment.CurrentDirectory, "Wordle.Tests/Data/words_test.txt");
+    private static string relativePath = "../../../Data/words_test.txt";
+    private static string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+    public static string filePath = Path.GetFullPath(Path.Combine(currentDirectory, relativePath));
 
-// public static string fileName = @"/Users/praveen/Desktop/images\November.pdf";  
-    // FileInfo fi = new FileInfo(fileName); 
-    // public static string relativePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location).ToString();
     [Fact]
     public void ReadWordsFileIntoList_WhenFilePathIsNotNull_ShouldReturnFileWordsInList()
     {
@@ -45,14 +38,14 @@ public class AnswerUnitTests
 
         var actualResult = answer.ReadWordsFile(filePath);
         bool isEqualLists = expectedResult.All(actualResult.Contains) && actualResult.All(expectedResult.Contains);
-        
+
         Assert.True(isEqualLists);
     }
 }
 
 
-    // private static string _WordFileName = @"word.txt";
-    // public static string CurrentPath
-    // {
-    //     get
-    // }"Wordle.Test.Data.words_test.txt"
+// private static string _WordFileName = @"word.txt";
+// public static string CurrentPath
+// {
+//     get
+// }"Wordle.Test.Data.words_test.txt"
