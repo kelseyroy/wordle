86892ad1146556b2c7710266687ab8c0245d6c01
Kelsey Roy <kroy@8thlight.com>
2023-05-15 10:43:42 -0700

    Adds IWordleUI interface

---
+++ Wordle.UI/IWordleUI.cs
@@ -0,0 +1,11 @@
+using Wordle.Domain;
+namespace Wordle.UI;
+
+public interface IWordleUI
+{
+    public string GetGuess();
+    void UpdateBoard(WordScore[] words, int guessCount);
+    void DisplayEmptyBoard();
+    string GetGuessInput();
+    void DisplayMessage(string message);
+}
