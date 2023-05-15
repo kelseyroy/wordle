d07b7f80f0808050be91759f7e7395fbd41019b4
Kelsey Roy <kroy@8thlight.com>
2023-05-15 13:36:46 -0700

    Adds to switch statement to MainMenu and finally statement to quit app

--- Wordle.UI/Program.cs
+++ Wordle.UI/Program.cs
@@ -11,23 +11,66 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        NewGame();
+        try
+        {
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+        }
+        catch (Exception error)
+        {
+            Console.WriteLine(error.Message);
+            throw;
+        }
+        finally
+        {
+            Console.ResetColor();
+            Console.Clear();
+            Console.WriteLine("Thanks for playing! Wordle was closed.");
+        }
+
+
+        // NewGame();
     }
-    private static void MainMenu()
+    private static bool MainMenu()
     {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.Black;
         Console.Clear();
         Console.WriteLine("Welcome to");
-        Console.WriteLine("╔═══╦═══╦═══╦═══╦═══╦═══╗");
-        ConsoleColor currentForeground = Console.ForegroundColor;
-        ConsoleColor currentBackground = Console.BackgroundColor;
         Logo(Console.ForegroundColor, Console.BackgroundColor);
-        Console.Write("║" + Environment.NewLine);
-        Console.WriteLine("╚═══╩═══╩═══╩═══╩═══╩═══╝");
+        Console.WriteLine("Choose an option from the following list and hit enter:");
+        Console.WriteLine("\t[ -g | --game ] to play the game");
+        Console.WriteLine("\t[ -h | --help ] for usage/instructions");
+        Console.WriteLine("\t[ -q | --quit ] to quit");
+        Console.WriteLine("What would you like to do?");
+        switch (Console.ReadLine())
+        {
+            case "-g":
+            case "--game":
+                NewGame();
+                return true;
+            case "-h":
+            case "--help":
+                HelpMenu();
+                return true;
+            case "-q":
+            case "--quit":
+                return false;
+            default:
+                return true;
+        }
+    }
+    private static void HelpMenu()
+    {
+        Console.WriteLine("Help Menu to come");
     }
     private static void Logo(ConsoleColor foreground, ConsoleColor background)
     {
         ConsoleColor[] colors = new ConsoleColor[] { ConsoleColor.DarkGreen, ConsoleColor.DarkGray, ConsoleColor.DarkYellow };
-
+        Console.WriteLine("╔═══╦═══╦═══╦═══╦═══╦═══╗");
         foreach (char letter in "WORDLE")
         {
             Console.Write("║");
@@ -39,49 +82,44 @@ public static class Program
             Console.ForegroundColor = foreground;
             Console.BackgroundColor = background;
         }
+        Console.Write("║" + Environment.NewLine);
+        Console.WriteLine("╚═══╩═══╩═══╩═══╩═══╩═══╝" + Environment.NewLine);
     }
     private static void NewGame()
     {
-        try
+        Game game = new Game(null);
+        ConsoleUI consoleUI = new ConsoleUI();
+        string currentGuess = "";
+        GameState gameState = GameState.Playing;
+
+        consoleUI.DisplayEmptyBoard();
+        while (gameState == GameState.Playing)
         {
-            Game game = new Game(null);
-            ConsoleUI consoleUI = new ConsoleUI();
-            string currentGuess = "";
-            GameState gameState = GameState.Playing;
+            // Console.WriteLine(game.GetAnswer());
+            gameState = TakeTurn();
+        }
 
-            consoleUI.DisplayEmptyBoard();
-            while (gameState == GameState.Playing)
+        Dictionary<int, WordScore> GetGuess()
+        {
+            consoleUI.DisplayMessage("Type in your 5 letter guess, then hit enter:");
+            currentGuess = consoleUI.GetGuessInput();
+            if (game.IsMoveAccepted(currentGuess, out Dictionary<int, WordScore>? guesses))
             {
-                // Console.WriteLine(game.GetAnswer());
-                gameState = TakeTurn();
+                return guesses;
             }
-
-            Dictionary<int, WordScore> GetGuess()
+            else
             {
-                consoleUI.DisplayMessage("Type in your 5 letter guess, then hit enter:");
-                currentGuess = consoleUI.GetGuessInput();
-                if (game.IsMoveAccepted(currentGuess, out Dictionary<int, WordScore>? guesses))
-                {
-                    return guesses;
-                }
-                else
-                {
-                    consoleUI.DisplayMessage("Invalid word.");
-                    return GetGuess();
-                }
-            }
-            GameState TakeTurn()
-            {
-                WordScore[] guesses = new WordScore[6];
-                Dictionary<int, WordScore> getGuesses = GetGuess();
-                getGuesses.Values.ToArray().CopyTo(guesses, 0);
-                consoleUI.UpdateBoard(guesses);
-                return game.EvaluateGameState(currentGuess);
+                consoleUI.DisplayMessage("Invalid word.");
+                return GetGuess();
             }
         }
-        catch (Exception error)
+        GameState TakeTurn()
         {
-            Console.WriteLine(error.Message);
+            WordScore[] guesses = new WordScore[6];
+            Dictionary<int, WordScore> getGuesses = GetGuess();
+            getGuesses.Values.ToArray().CopyTo(guesses, 0);
+            consoleUI.UpdateBoard(guesses);
+            return game.EvaluateGameState(currentGuess);
         }
 
     }
